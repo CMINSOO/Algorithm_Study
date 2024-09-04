302034a0fa1eec971a17f61c626e2dd8b646abb9
CMINSOO <165575863+CMINSOO@users.noreply.github.com>
2024-09-04 16:16:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,11 @@
+function solution(A, B) {
+    var arr = [...A]
+    if(A === B) return 0
+    for (let i = 0; i < arr.length; i++){
+            arr.unshift(arr.pop())
+            if(arr.join("") === B){
+                return i + 1
+            }
+    }
+    return -1
+}
