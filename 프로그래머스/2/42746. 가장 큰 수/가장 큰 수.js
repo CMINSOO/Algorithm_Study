d2b907ac5c6c859fb60da44740adccc326db5a28
CMINSOO <165575863+CMINSOO@users.noreply.github.com>
2024-10-02 02:01:14 +0900

    [level 2] Title: 가장 큰 수, Time: 185.26 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,13 @@
 function solution(numbers) {
-    var answer = '';
+    let answer = '';
+    //1. numbers들을 전부 string 형식으로 바꿔주기
+    //2. 문자열화된 numbers를 합쳤을때 큰순으로 정렬하기
+    //3. 배멸이 0으로만 이루어질수 있는 경우를 예외처리하기
+    const stringfiedNumbers = numbers.map(numbers => String(numbers))
+    console.log(stringfiedNumbers)
+    answer = stringfiedNumbers.sort((a,b) =>(b+a) - (a+b)).join('')
+    if(answer[0] == "0"){
+        return "0"
+    }
     return answer;
 }
