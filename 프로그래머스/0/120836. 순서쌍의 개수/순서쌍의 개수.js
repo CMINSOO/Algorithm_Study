593f91273e7bc4269b837863619e94d18bbbd566
CMINSOO <165575863+CMINSOO@users.noreply.github.com>
2024-12-19 18:12:58 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.06 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let index = 1
+    let arr = []
+    while(index <= n){
+        if(n % index == 0) arr.push(index)
+        index++
+    }
+    
+    return arr.length
+}
